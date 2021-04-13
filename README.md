cf60a9cc435be1b68c8d23e3fba91897457876a2
Abned <fanabned@gmail.com>
2021-04-13 11:21:20 +0200

    feat: create and configure project

---
+++ README.md
@@ -0,0 +1,5 @@
+## Symfony Testing
+### class utilities
+* TestCase
+* KelnelTestCase : test fonctionnelle
+* WebTestCase: testing a controller
---
+++ tests/AppTest.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class AppTest extends TestCase {
+    public function testTestsAreWorking() {
+        $this->assertTrue(true, 'Test configuration should work');
+    }
+}
